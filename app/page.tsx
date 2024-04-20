dd08b5d441fc7369a879298919679fc4a8b70570
th3man1998 <th3man1998@gmail.com>
2024-04-20 12:51:19 +0000

    Create App skeleton

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <h1>Create App Skeleton.</h1>
+   <p>Start Coding the Notion Clone Features.</p>
+   </>
+  );
+}
